12d45d51ad03a3ddc89c9953898ad24feb2fd2e6
Justas <justasvee@gmail.com>
2020-12-29 12:31:31 +0200

    OOP form validation

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+    require('user_validator.php');
+
+    if(isset($_POST['submit'])) {
+        //validate entries
+
+        $validation = new UserValidator($_POST);
+        $errors = $validation->validateForm();
+
+        //save data to db
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="new-user">
+        <h2>Create new user</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+            <label>Username:</label>
+            <input type="text" name="username" value="<?php echo htmlspecialchars($_POST['username']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['username'] ?? '' ?>
+            </div>
+
+            <label>Email:</label>
+            <input type="text" name="email" value="<?php echo htmlspecialchars($_POST['email']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['email'] ?? '' ?>
+            </div>
+
+            <input type="submit" value="submit" name="submit">
+        </form>
+    </div>
+</body>
+</html>
